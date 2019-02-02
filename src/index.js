d40bcd9e761ef137725f9a6bf218d2096af7415e
Lucas Cherkewski <lucas@ecustom.ca>
2019-02-02 18:56:52 -0500

    add URL library

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const fetch = require('node-fetch');
 
+const URL = require("url").URL;
+
 const { saveToFirestore } = require('./lib/firestore');
 const { publishMessageToPubSub } = require('./lib/pubsub');
 const { extractPlaqueSlug, extractPlaqueData } = require('./lib/extractors/plaqueDetails');
 const { extractPlaquePageUrls } = require('./lib/extractors/indexPageUrls');
 
 exports.getPlaqueDataFromOhtPage = async (data, context) => {
+    const urlToScrape = new URL(Buffer.from(data.data, 'base64').toString());
+
     console.log(data);
     console.log(Buffer.from(data.data, 'base64').toString());
+    console.log(urlToScrape);
 
-    const ohtResult = await fetch(Buffer.from(data.data, 'base64').toString());
+    const ohtResult = await fetch(urlToScrape);
 
     const responseBody = await ohtResult.text();
 
