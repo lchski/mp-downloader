4ad1ff52fc9ca6581ac30adc32cec9302fac901e
Lucas Cherkewski <lucas@ecustom.ca>
2019-02-01 22:35:59 -0500

    extract more info

--- index.js
+++ index.js
@@ -13,7 +13,15 @@ exports.getPlaqueDataFromOhtPage = async (req, res) => {
     const plaqueSection = $('#content');
 
     plaqueDetails.title = plaqueSection.find('h1').text();
-    plaqueDetails.text = plaqueSection.find('section:first-of-type p').text();
+    plaqueDetails.text = plaqueSection.find('section p').first().text();
+
+    const locationSection = plaqueSection.find('h3:contains("Location")').parent();
+
+    plaqueDetails.location = {};
+    plaqueDetails.location.address = locationSection.find('p').first().text();
+    plaqueDetails.location.region = locationSection.find('div p:nth-child(1) a').text();
+    plaqueDetails.location.county = locationSection.find('div p:nth-child(2) a').text();
+    plaqueDetails.location.municipality = locationSection.find('div p:nth-child(3) a').text();
 
     const resJson = {
         status: ohtResult.status,
