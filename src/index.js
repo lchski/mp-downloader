ccdb5583fe321f3d3eca80a321d79d5907362d2f
Lucas Cherkewski <lucas@ecustom.ca>
2019-02-02 19:24:55 -0500

    remove console.logs

--- src/index.js
+++ src/index.js
@@ -10,10 +10,6 @@ const { extractPlaquePageUrls } = require('./lib/extractors/indexPageUrls');
 exports.getPlaqueDataFromOhtPage = async (data, context) => {
     const urlToScrape = new URL(JSON.parse(Buffer.from(data.data, 'base64').toString()));
 
-    console.log(data);
-    console.log(Buffer.from(data.data, 'base64').toString());
-    console.log(urlToScrape);
-
     const ohtResult = await fetch(urlToScrape);
 
     const responseBody = await ohtResult.text();
