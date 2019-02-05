77db1f872dd55fbafd5728d88bc857fcce953e2b
Lucas Cherkewski <lucas@ecustom.ca>
2019-02-04 22:03:13 -0500

    some work on functions to extract row details

--- src/download.js
+++ src/download.js
@@ -52,9 +52,40 @@ const { getAllRecordsInFirestoreCollection } = require('./lib/firestore');
 const convertRecordsToTsv = (records) => {
     let rows = addRow([], ['plaqueId', 'category', 'key', 'value']);
 
+    records.forEach((record) => {
+        console.log(extractDetailRows(record.details));
+    })
+
     return rows;
 };
 
 const addRow = (existingRows, newRowItems) => {
     return existingRows += newRowItems.join('\t') + '\r\n';
+};
+
+const extractDetailRows = (detailItems) => {
+    let rows = [
+        ['title', detailItems.title],
+        ['text', detailItems.text],
+    ];
+
+    rows = rows.map((row) => pushToBeginningOfArray(row, 'details'));
+
+    return rows;
+};
+
+const extractLocationRows = (locationItems) => {
+
+};
+
+const extractThemeRows = (themeItems) => {
+
+};
+
+const pushToBeginningOfArray = (arrayToModify, itemToPush) => {
+    let arrayCopy = [...arrayToModify];
+
+    arrayCopy.unshift(itemToPush);
+
+    return arrayCopy;
 };
